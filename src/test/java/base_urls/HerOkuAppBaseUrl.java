ae9491528dc921a69d14ffb3ac3ee2d091e19bed
HaticeBilici61 <bilicihatice1987@gmail.com>
2023-03-20 21:30:30 +0100

    a

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -1,5 +1,6 @@
 package base_urls;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
@@ -9,6 +10,6 @@ public class HerOkuAppBaseUrl {
 
     @Before//Her test methodundan önce çalışır.
     public void setUp() {
-        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
     }
 }
